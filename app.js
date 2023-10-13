0c2c187163b04adb2d352d7b38fd4745ec09998f
Shyam3050 <nimmanashyam3050@gmail.com>
2023-10-13 17:26:04 +0530

    vercel json file added

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const restaurantRoute = require("./routes/restaurentRoute");
 
 app.use("/api/restaurents", restaurantRoute);
 
+app.use("/", (req, res) =>
+  res.status(400).json({
+    data: "mainfile",
+  })
+);
 module.exports = app;
