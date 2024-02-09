c15e190ae97d06ea5c525d17c533d796ced27d58
Shyam3050 <nimmanashyam3050@gmail.com>
2024-02-09 10:54:01 +0530

    cors policy

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const restaurantRoute = require("./routes/restaurentRoute");
+const cors = require('cors')
 
 app.use("/api/restaurents", restaurantRoute);
+
+app.use(cors());
+app.options("*", cors());
 
 app.use("/", (req, res) =>
   res.status(400).json({
